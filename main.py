5c965163679034caba14407210ec3704a8f2be93
SerahTheLioness <51043629+SerahTheLioness@users.noreply.github.com>
2020-11-08 02:35:37 -0800

    Update main.py
    OOPS! I'm coding at 2:35 am!

--- main.py
+++ main.py
@@ -3,13 +3,27 @@ import time
 from RPLCD.i2c import CharLCD
 import sys
 
+#NOTE: 0x27 may be different on your screen, refer to https://rplcd.readthedocs.io/
 lcd = CharLCD('PCF8574', 0x27)
 lcd.backlight_enabled = True
 
 def main():
-    foo = input("Type today's code: ")
+    time.sleep(0.05)
+    # this sleep ensures no funny business with the restart
+    lcd.clear()
+    lcd.write_string("Input code\n\ron host...")
+    foo = input("Input today\'s code: ")
+    lcd.clear()
     lcd.write_string(foo)
-    time.sleep(999)
+    time.sleep(60)
+    lcd.clear()
+    t = 5
+    while t > 0:
+        lcd.clear()
+        lcd.write_string("LCD_timeout" + "\n\r" + "Timeout: " + str(t))
+        t -= 1
+        time.sleep(1)
+    
     
 while True:
     if __name__ == "__main__":
